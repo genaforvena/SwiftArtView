36ed86e089fb5b125df2414c8841954e879e8cb7
Ilya Mozerov <imozerov@ccc-team-mac1.local>
2016-05-12 18:33:35 +0300

    Initial Commit

---
+++ SwiftArtView/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SwiftArtView
+//
+//  Created by Ilya Mozerov on 5/12/16.
+//  Copyright © 2016 street art view. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
