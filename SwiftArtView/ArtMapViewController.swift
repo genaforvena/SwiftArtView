2435a4442f7b69f7e28ea7c221877b78ced14a60
Ilya Mozerov <imozerov@ccc-team-mac1.local>
2016-05-13 15:50:42 +0300

    Added markers to the map

--- SwiftArtView/ArtMapViewController.swift
+++ SwiftArtView/ArtMapViewController.swift
@@ -9,13 +9,58 @@
 import UIKit
 import MapKit
 
-class ArtMapViewController: UIViewController {
+class ArtMapViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet var mapView: MKMapView!
+    
+    var artWorks: [ArtWork] = [] {
+        didSet {
+            artWorks.forEach { artwork in
+                mapView.addAnnotation(ArtWorkAnnotation(artWork: artwork))
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        fetchArtWorks()
     }
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        if let annotation = annotation as? ArtWorkAnnotation {
+            let identifier = "pin"
+            var view: MKPinAnnotationView
+            if let dequeuedView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier)
+                as? MKPinAnnotationView { // 2
+                dequeuedView.annotation = annotation
+                view = dequeuedView
+            } else {
+                // 3
+                view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                view.canShowCallout = true
+                view.calloutOffset = CGPoint(x: -5, y: 5)
+            }
+            return view
+        }
+        return nil
+    }
+    
+    // We need to upload the contacts before this.
+    func fetchArtWorks() {
+        StreetArtViewAPI.sharedInstance.getArtWorksList() { artWorks in
+            self.artWorks = artWorks
+        }
+    }
+}
+
+class ArtWorkAnnotation : NSObject, MKAnnotation {
+    init(artWork: ArtWork) {
+        coordinate = CLLocationCoordinate2D.init(latitude: artWork.location.lat, longitude: artWork.location.lng)
+        title = artWork.name
+    }
+    
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
 }
 
